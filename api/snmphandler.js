f51275448dcf4007e6e8ee41510a8110d3280d83
aPurpleDev <chemseddine.henni@live.fr>
2019-11-08 10:19:45 +0100

    getting total RAM size now

--- api/snmphandler.js
+++ api/snmphandler.js
@@ -1,13 +1,26 @@
 const chalk = require('chalk');
-const snmp = require('snmp-native');
 
-const session = new snmp.Session({host: 'localhost', port: 8060});
+const snmp = require('net-snmp');
+const session = snmp.createSession('192.168.10.148', 'public');
+const oids = ['1.3.,6.1.2.1.25.2.3.1.3.1'];
 
-session.get({oid: [1, 3, 6, 1, 2, 1, 25, 2, 3, 1], community: 'public'}, (error, variables) => { //not working, possibly because snmpwalk is not available on this OS
-    if (error) {
-        console.log(chalk.red('Error fetching OID variables'), error.message);
-    } else {
-        console.log(chalk.greenBright(`${variables[0].oid} = ${variables[0].value} (et de type ${variables[0].type})`));
+session.get(oids, (error, varbinds) => {
+    if(error){
+        console.log(chalk.red('error snmp'),error.message);
+    }else{
+        for( let i = 0; i < varbinds.length; i++){
+            if(snmp.isVarbindError(varbinds[i])){
+                console.error(snmp.varbindError(varbinds[i]))
+            }else {
+                console.log(varbinds[i].oid + ' = ' + varbinds[i].value);
+            }
+        }
     }
+
+    session.close();
 });
 
+session.trap(snmp.TrapType.LinkDown, (error) => {
+    if(error)
+        console.error(error);
+});
